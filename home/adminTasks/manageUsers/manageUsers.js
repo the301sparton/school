25d8b3b0aa2685f55940ab5f317fa0b0349d0927
Chaitanya Deshpande <chaitanya.deshp@gmail.com>
2019-07-18 19:04:12 +0530

    add basic manageUser.js

--- home/adminTasks/manageUsers/manageUsers.js
+++ home/adminTasks/manageUsers/manageUsers.js
@@ -1,3 +1,22 @@
 function manageUsers(){
     setActiveColorsAdminTasks("manageUsers");
+   
+    let manageUsersHTML = `<div class="container">
+    <div class="text-center">
+          <h5>Manage Users</h5>
+          <hr>
+    </div>`;
+    document.getElementById('adminActionHolder').innerHTML = manageUsersHTML;
+    getUserList();
+
+}
+
+function getUserList(){
+    var getAllUsersReq = $.post(baseUrl + "/apis/User.php",{
+        type: "getAllUsers"
+    });
+
+    getAllUsersReq.done(function(allUserStr){
+        console.log(allUserStr);
+    });
 }
