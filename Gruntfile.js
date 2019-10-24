ad1d679844b282b2c1abe43f0a256496ec52dc14
Chaitanya Deshpande <chaitanya.deshp@gmail.com>
2019-10-24 23:23:48 +0530

    del

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,62 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
+  grunt.initConfig({
 
-        jshint: {
-            options: {
-              curly: true,
-              eqnull: true,
-              browser: true,
-              "esversion": 6,
-              globals: {
-                jQuery: true
-              },
-            },
-            files: ['home/**/*.js', 'js/*.js']
-        },
-        watch: {
-          js: {
-              files: ['home/**/*.js', 'js/*.js'], tasks: ['jshint'], options: {
-                  livereload: true,
-              }
-          }
-      }   
-    });
+    watch: {
+      js: {
+        files: ['home/**/*.js', 'js/*.js'], tasks: ['concat', 'obfuscator'], options: {
+          livereload: true,
+        }
+      }
+    },
 
-    // load plugins
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.registerTask('default', ['jshint']);
+
+    concat: {
+      options: {
+        separator: ';',
+      },
+
+      homeJS: {
+        src: ['home/**/*.js', './js/setAll.js', './js/firebaseAuth.js', './js/home_main.js', './js/main.js'],
+        dest: 'dist/builtHome.js',
+      },
+
+      indexJS: {
+        src: [ './js/firebaseAuth.js', './js/index_main.js', './js/main.js'],
+        dest: 'dist/builtIndex.js',
+      }
+
+    },
+
+
+    obfuscator: {
+      options: {
+        banner: '// Copyright vaicomp.com 2019 India.\n',
+        debugProtection: true,
+        debugProtectionInterval: true
+      },
+
+      homeObfuscator: {
+        files: {
+          'dist/': [ // the files and their directories will be created in this folder
+            'dist/builtHome.js'
+          ]
+        }
+      },
+
+      indexObfuscator: {
+        files: {
+          'dist/': [ // the files and their directories will be created in this folder
+            'dist/builtIndex.js'
+          ]
+        }
+      }
+
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-obfuscator');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['watch']);
 };
+
