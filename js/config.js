04ad1174be8f3314765cbe9e7d9ac3e4cd808d44
Chaitanya Deshpande <chaitanya.deshp@gmail.com>
2019-10-27 15:01:52 +0530

    release tested

--- js/config.js
+++ js/config.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-var baseUrl = "/School";
-var logOutUrl = "/School";
-var websiteTitle = "BES Gondia";
-var websiteDesc = "Best in class education for your children.";
-
-var TOSLink = "";
-var PPLink = "";
-var facebookLink = "";
-var twitterLink = "";
-var linkedinLink = "";
-var instagramLink = "";
-var client_email = "info@besgondia.com";
-var client_phone = "7219309093";
-var address = "Bengali School<br>Railtoil<br>Gondia - 441614<br><strong>Email:</strong> "+client_email+"<br><strong>Phone:</strong> "+client_phone;
-=======
 let verName = "1.5.0";
 let baseUrl = "/School";
 let logOutUrl = "/School";
@@ -29,5 +13,4 @@ let instagramLink = "";
 let client_email = "info@besgondia.com";
 let client_phone = "7219309093";
 let address = "Bengali School<br>Railtoil<br>Gondia - 441614<br><strong>Email:</strong> "+client_email+"<br><strong>Phone:</strong> "+client_phone;
->>>>>>> master
 
