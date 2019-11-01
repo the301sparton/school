4b47cb354d1d606875a2a8609cbf6b3ee6e6bf8e
Chaitanya Deshpande <chaitanya.deshp@gmail.com>
2019-11-02 01:16:43 +0530

    local firebaseUI libs

--- home/adminTasks/manageClassList/manageClassList.js
+++ home/adminTasks/manageClassList/manageClassList.js
@@ -1,3 +1,14 @@
 function manageClassList(){
     setActiveColorsAdminTasks("manageClassList");
+    let manageClassListHTML = `<div class="container">
+    <div class="text-center">
+          <h5>Manage Class List</h5>
+          <hr>
+    </div>
+     
+    <div class="container" id="manageHeadsHolder">
+    </div>
+    <div id="jsGrid" style = "display:none"></div>
+    </div>`;
+    document.getElementById('adminActionHolder').innerHTML = manageClassListHTML;
 }
