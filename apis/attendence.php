d690cbf8114ef8947849c4f0acbc06701f7d74d9
Chaitanya Deshpande <chaitanya.deshp@gmail.com>
2019-11-10 15:51:08 +0530

    prepaire for studentList (attendence)

---
+++ apis/attendence.php
@@ -0,0 +1,32 @@
+<?php
+require_once 'db.php';
+require_once 'commonFunctions.php';
+$type = $_POST['type'];
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+else{
+    if($type == "getList"){
+        $sessionName = $_POST['sessionName'];
+        $classFull = $_POST["class"];
+        $classsessionName = '$sessionName';
+        $classArray = explode(":", $classFull);
+        $class = $classArray[0];
+        $section = $classArray[1];
+        $dateForAttendence = $_POST["dateForAttendence"];
+        $dateForAttendenceFinal = date('Y-m-d', strtotime($dateForAttendence)); 
+        $sqlforAlready = "SELECT * FROM attendencedetails WHERE `sessionName` = '$sessionName' AND `date`= '$dateForAttendenceFinal' AND `class` = '$class'AND `section` = '$section'";
+        $result = mysqli_query($GLOBALS['conn'],$sqlforAlready);  
+        if (mysqli_num_rows($result) == 0) { 
+            $sql = "SELECT sessionName, class, section, studentid, firstName, middleName, lastName FROM searchstudent WHERE `sessionName` = '$sessionName' AND `class` = '$class'AND `section` = '$section'";
+            getOutputFromQueary($sql);
+        }
+        else{
+            getOutputFromQueary($sqlforAlready);
+        }    
+    }
+}
+
+?>
--- home/studentStuff/studentAttendence/studentAttendence.js
+++ home/studentStuff/studentAttendence/studentAttendence.js
@@ -1,7 +1,7 @@
-function studentAttendence(){
-    currentStudentOption = "studentAttendence";
-    setActiveColorsStudent("studentAttendence");
-    document.getElementById('studentActionHolder').innerHTML = `<div class="container" id="registerStudent">
+function studentAttendence() {
+  currentStudentOption = "studentAttendence";
+  setActiveColorsStudent("studentAttendence");
+  document.getElementById('studentActionHolder').innerHTML = `<div class="container" id="registerStudent">
     <div class="text-center">
       <h4 id="searchHeading">Student Attendence</h4>
       <hr>
@@ -54,50 +54,58 @@ function studentAttendence(){
                 <h4 id="classDetailsForAttendence"></h4>
                 <hr>
             </div>
+            
+            <div class = "row">
+                  <div class="col-md-3"></div>
 
-           
+                  <div class="col-md-6">
+                  <div id="myListHolder" class="container"></div>
+                  </div>
+
+                  <div class="col-md-3"></div>
+            </div>
     </div>
 
 
     </div>`;
 
-loadAttendenceViewData();
+  loadAttendenceViewData();
 }
 
-function loadAttendenceViewData(){
-    document.getElementById('loader').style.display = "block";
-    $.when(loadAllSessionsForAttendence(), loadClassListWithAccess()).then(function(){
-        document.getElementById('loader').style.display = "none";
-    });
+function loadAttendenceViewData() {
+  document.getElementById('loader').style.display = "block";
+  $.when(loadAllSessionsForAttendence(), loadClassListWithAccess()).then(function () {
+    document.getElementById('loader').style.display = "none";
+  });
 }
 
 
 
 function loadAllSessionsForAttendence() {
-    var allSessionReq = $.post(baseUrl + "/apis/academicSession.php", {
-      type: "getAllSessions"
-    });
-  
-    allSessionReq.done(function (allSessions) {
-      allSessions = JSON.parse(allSessions);
-      for (var index in allSessions) {
-  
-        $('#attendence_sessionName')
-          .append($('<option>', { value: allSessions[index].sessionName })
-            .text(allSessions[index].sessionName
-            ));
-      }
-  
+  var allSessionReq = $.post(baseUrl + "/apis/academicSession.php", {
+    type: "getAllSessions"
+  });
+
+  allSessionReq.done(function (allSessions) {
+    allSessions = JSON.parse(allSessions);
+    for (var index in allSessions) {
+
+      $('#attendence_sessionName')
+        .append($('<option>', { value: allSessions[index].sessionName })
+          .text(allSessions[index].sessionName
+          ));
+    }
+
     document.getElementById("attendence_sessionName").value = currentSession;
     sessionSelect = currentSession;
-    
-    
-    });
 
-    allSessionReq.fail(function(jqXHR, textStatus){handleNetworkIssues(textStatus)});
+
+  });
+
+  allSessionReq.fail(function (jqXHR, textStatus) { handleNetworkIssues(textStatus) });
 }
 
-function loadClassListWithAccess(){
+function loadClassListWithAccess() {
   var allClassReq = $.post(baseUrl + "/apis/classList.php", {
     type: "getAllCLassWithAccess",
     uid: me_data.uid
@@ -108,35 +116,66 @@ function loadClassListWithAccess(){
     for (var index in allClass) {
 
       $('#attendence_className')
-        .append($('<option>', { value: allClass[index].className+allClass[index].section })
+        .append($('<option>', { value: allClass[index].className + ":" + allClass[index].section })
           .text(allClass[index].className + " " + allClass[index].section
           ));
     }
   });
 
-  allClassReq.fail(function(jqXHR, textStatus){handleNetworkIssues(textStatus)});
+  allClassReq.fail(function (jqXHR, textStatus) { handleNetworkIssues(textStatus) });
 }
 
 
-function getStudentListForAttendence(){
+function getStudentListForAttendence() {
   let sessionName = document.getElementById("attendence_sessionName").value;
   let classNSection = document.getElementById("attendence_className").value;
   let dateForAttendence = document.getElementById("attendence_date").value;
 
-  if(sessionName != "" && classNSection != "" && dateForAttendence != ""){
+  if (sessionName != "" && classNSection != "" && dateForAttendence != "") {
     var date = new Date(dateForAttendence);
-    if( date.setHours(0,0,0,0) > new Date().setHours(0,0,0,0)){
+    if (date.setHours(0, 0, 0, 0) > new Date().setHours(0, 0, 0, 0)) {
       document.getElementById("attendence_alert").innerText = "Can not set attendence for future dates";
       document.getElementById("attendence_alert").style.display = "block";
     }
-    else{
+    else {
       document.getElementById("attendence_alert").style.display = "none";
+      getAttendenceList(sessionName, classNSection, dateForAttendence);
     }
-    
+
 
   }
-  else{
+  else {
     document.getElementById("attendence_alert").innerText = "Please Select All Values";
     document.getElementById("attendence_alert").style.display = "block";
   }
+}
+
+
+function getAttendenceList(sessionName, classNSection, dateForAttendence) {
+  let getAttendenceListReq = $.post(baseUrl + "/apis/attendence.php", {
+    type: "getList",
+    sessionName: sessionName,
+    class: classNSection,
+    dateForAttendence: dateForAttendence
+  });
+
+  getAttendenceListReq.done(function (response) {
+    var studList = JSON.parse(response);
+    arrayData = new Array();
+    for (student in studList) {
+      var obj = new Object;
+      obj.fullname = studList[student].firstName + " " + studList[student].middleName + " " + studList[student].lastName;
+      if(studList[student].state != null && studList[student].state != ""){
+        obj.state = studList[student].state;
+      }
+      else{
+        obj.state = false;
+      }
+      arrayData.push(obj);
+    }
+
+  });
+
+  getAttendenceListReq.fail(function (jqXHR, textStatus) { handleNetworkIssues(textStatus) });
+
 }
