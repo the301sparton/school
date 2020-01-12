1119dec697edb9c9c1071da644104c5fb498ec19
root <root@pop-os.localdomain>
2020-01-12 23:42:23 +0530

    prod push prep v1.5.1

--- js/config.js
+++ js/config.js
@@ -3,7 +3,6 @@ let baseUrl = "";
 let logOutUrl = "www.besgondia.org";
 let websiteTitle = "BES Gondia";
 let websiteDesc = "Best in class education for your children.";
-
 let TOSLink = "";
 let PPLink = "";
 let facebookLink = "";
@@ -12,4 +11,5 @@ let linkedinLink = "";
 let instagramLink = "";
 let client_email = "info@besgondia.com";
 let client_phone = "7219309093";
+
 let address = "Bengali School<br>Railtoil<br>Gondia - 441614<br><strong>Email:</strong> "+client_email+"<br><strong>Phone:</strong> "+client_phone;
